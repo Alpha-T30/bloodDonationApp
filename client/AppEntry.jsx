c16f654b6ab7aa99e2011146d24cb6f8969c3a57
Enamul <enamult300@gmail.com>
2022-06-11 05:03:39 +0600

    search bar added

--- client/AppEntry.jsx
+++ client/AppEntry.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -10,46 +11,188 @@ import {
   initialWindowMetrics,
 } from "react-native-safe-area-context";
 
-import { View, Text } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./src/screens/home";
 import Login from "./src/screens/login";
 import { useSelector } from "react-redux";
 import Register from "./src/screens/register";
+import {
+  CheckIcon,
+  HamburgerIcon,
+  Input,
+  Menu,
+  Pressable,
+  Select,
+  NativeBaseProvider,
+  extendTheme,
+} from "native-base";
+import SearchBar from "react-native-dynamic-search-bar";
+import { AntDesign } from "@expo/vector-icons";
+import { bloodGroup } from "./Data/data";
+import { useState } from "react";
 
+const theme = extendTheme({
+  components: {
+    Select: {
+      // Can simply pass default props to change default behaviour of components.
+      baseStyle: {
+        borderColor: "rgba(0, 0, 0, 0)",
+      },
+    },
+  },
+});
 export default function AppEntry() {
+  const [bGroup, setbGroup] = useState("");
   const currentUser = useSelector(
     (state) => state.persistedData.auth?.currentUser
   );
   const isRegisterd = useSelector((state) => state.ui.isRegisterd);
   const Stack = createNativeStackNavigator();
+
+  const searchFun = (text) => {
+    console.log(text);
+  };
+  const handleMenu = () => {
+    console.log("hello clicked");
+  };
+  const handlegbSelect = (bg) => {
+    setbGroup(bg);
+  };
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {!currentUser ? (
-          <>
-            <Stack.Screen
-              options={{ headerShown: false }}
-              name="Login"
-              component={Login}
-            />
-            <Stack.Screen
-              options={{ headerShown: false }}
-              name="Register"
-              component={Register}
-            />
-          </>
-        ) : (
-          <>
-            <Stack.Screen
-              options={{ title: "Home Page" }}
-              name="Home"
-              component={Home}
-            />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {!currentUser ? (
+            <>
+              <Stack.Screen
+                options={{ headerShown: false }}
+                name="Login"
+                component={Login}
+              />
+              <Stack.Screen
+                options={{ headerShown: false }}
+                name="Register"
+                component={Register}
+              />
+            </>
+          ) : (
+            <>
+              <Stack.Screen
+                options={{
+                  headerStyle: {
+                    backgroundColor: "crimson",
+                    elevation: 20,
+                  },
+                  headerTitle: () => (
+                    <View style={styles.topBar}>
+                      <View style={styles.filter}>
+                        <Select
+                          selectedValue={bGroup}
+                          mx={{
+                            base: 0,
+                            md: "auto",
+                          }}
+                          onValueChange={(nextValue) =>
+                            handlegbSelect(nextValue)
+                          }
+                          _selectedItem={{
+                            bg: "cyan.600",
+                          }}
+                          accessibilityLabel="Select a blood group"
+                        >
+                          <Select.Item label={"All"} value={""} />
+
+                          {bloodGroup.map((b) => (
+                            <Select.Item label={b.name} value={b.name} />
+                          ))}
+                        </Select>
+                      </View>
+                      <View style={styles.searchbar}>
+                        <SearchBar
+                          // spinnerVisibility={true}
+                          placeholder="Search here.."
+                          fontFamily="BurbankBigCondensed-Black"
+                          // shadowStyle={styles.searchBarShadowStyle}
+                          // darkMode={true}
+                          onChangeText={searchFun}
+                          height={50}
+                          fontSize={17}
+                          fontColor="#fdfdfd"
+                          iconColor="#fdfdfd"
+                          shadowColor="red"
+                          cancelIconColor="#fdfdfd"
+                          // backgroundColor="#ba312f"
+                          spinnerVisibility={true}
+                          shadowStyle={styles.searchBarShadowStyle}
+                        />
+                      </View>
+                      <View style={styles.menu}>
+                        <Menu
+                          shadow={2}
+                          w="190"
+                          trigger={(triggerProps) => {
+                            return (
+                              <Pressable
+                                accessibilityLabel="More options menu"
+                                {...triggerProps}
+                              >
+                                <HamburgerIcon
+                                  style={{ height: 30, width: 30 }}
+                                />
+                              </Pressable>
+                            );
+                          }}
+                        >
+                          <Menu.Item onPress={handleMenu}>Log Out</Menu.Item>
+                          <Menu.Item>Profile</Menu.Item>
+                        </Menu>
+                      </View>
+                    </View>
+                  ),
+                }}
+                name="Home"
+                component={Home}
+              />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  topBar: {
+    // width:100,
+    display: "flex",
+    flexDirection: "row",
+
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginLeft: -22,
+    marginTop: 10,
+  },
+  filter: {
+    flex: 1,
+
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  searchbar: {
+    flex: 5,
+    // backgroundColor: "yellow",
+    marginLeft: -10,
+  },
+  menu: {
+    flex: 1,
+    // backgroundColor: "green",
+    marginRight: 10,
+  },
+  searchBarShadowStyle: {
+    elevation: 20,
+    shadowColor: "#52006A",
+  },
+});
--- client/src/screens/home.jsx
+++ client/src/screens/home.jsx
@@ -14,20 +14,47 @@ import {
   NativeBaseProvider,
   FormControl,
   Input,
+  Select,
+  CheckIcon,
 } from "native-base";
+import { bloodGroup, districts, subDistricts } from "../../Data/data";
+
 export default function Home({ navigation }) {
   const [showModal, setShowModal] = useState(false);
   const [showBtn, setShowBtn] = useState(true);
   const [isLoading, setLoading] = useState(false);
+  const [formData, setData] = React.useState({});
+  const [bloodGroupselect, setBloodGroup] = React.useState([]);
+
   const user = useSelector(
     (state) => state.persistedData.auth?.currentUser?.userData
   );
   const allUsers = useSelector((state) => state.user.allUsers);
   const dispatch = useDispatch();
-  console.log("all users", allUsers);
+
   useEffect(() => {
     getAllUsers(dispatch, setLoading);
   }, [dispatch]);
+
+  const onSubmit = () => {
+    const findblood = bloodGroup.find((i) => i.id === bloodGroupselect);
+    // const findDistrict = districts.find((i) => i.id === districtSelect);
+    // const findSubDistrict = subDistricts.find((i) => i.id === singleSub);
+
+    const sendingData = {
+      ...formData,
+      bloodGroupId: findblood?.id,
+      bloodGroup: findblood?.name,
+      password: formData.password ? formData.password : "123456",
+      // districtId: districtSelect,
+      // subDistrictId: singleSub,
+
+      // district: findDistrict.name,
+      // subDistrict: findSubDistrict.name,
+    };
+    console.log(sendingData);
+    // register(sendingData, setLoading, navigation, toast);
+  };
   return (
     <View
       style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}
@@ -36,7 +63,7 @@ export default function Home({ navigation }) {
         return <CustomCard userData={user} key={i}></CustomCard>;
       })}
 
-      <Button title="Log Out" onPress={() => logOut(dispatch)} />
+      {/* <Button title="Log Out" onPress={() => logOut(dispatch)} /> */}
 
       <TouchableOpacity
         style={styles.addBtn}
@@ -48,23 +75,67 @@ export default function Home({ navigation }) {
       </TouchableOpacity>
 
       <Modal
+        safeAreaTop={true}
         isOpen={showModal}
         onClose={() => {
           setShowModal(false);
           setShowBtn(true);
         }}
       >
-        <Modal.Content size={"lg"}>
+        <Modal.Content size={"full"}>
           <Modal.CloseButton />
-          <Modal.Header>Contact Us</Modal.Header>
+          <Modal.Header>Add New User</Modal.Header>
           <Modal.Body>
             <FormControl>
               <FormControl.Label>Name</FormControl.Label>
-              <Input />
+              <Input
+                onChangeText={(value) => setData({ ...formData, name: value })}
+              />
             </FormControl>
-            <FormControl mt="3">
-              <FormControl.Label>Email</FormControl.Label>
-              <Input />
+            <FormControl>
+              <FormControl.Label>Mobile</FormControl.Label>
+              <Input
+                onChangeText={(value) =>
+                  setData({ ...formData, mobile: value })
+                }
+              />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Student ID</FormControl.Label>
+              <Input
+                onChangeText={(value) =>
+                  setData({ ...formData, studentId: value })
+                }
+              />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Session</FormControl.Label>
+              <Input
+                onChangeText={(value) =>
+                  setData({ ...formData, session: value })
+                }
+              />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Blood Group</FormControl.Label>
+              <Select
+                selectedValue={bloodGroupselect}
+                minWidth="200"
+                accessibilityLabel="Select Blood Group"
+                placeholder="Select Blood Group"
+                _selectedItem={{
+                  bg: "teal.600",
+                  endIcon: <CheckIcon size="5" />,
+                }}
+                mt={1}
+                onValueChange={(itemValue) => setBloodGroup(itemValue)}
+              >
+                {bloodGroup.map((item, i) => {
+                  return (
+                    <Select.Item key={i} label={item.name} value={item.id} />
+                  );
+                })}
+              </Select>
             </FormControl>
           </Modal.Body>
           <Modal.Footer>
@@ -79,14 +150,7 @@ export default function Home({ navigation }) {
               >
                 Cancel
               </Button>
-              <Button
-                onPress={() => {
-                  setShowModal(false);
-                  setShowBtn(true);
-                }}
-              >
-                Save
-              </Button>
+              <Button onPress={onSubmit}>Save</Button>
             </Button.Group>
           </Modal.Footer>
         </Modal.Content>
