72ce3fc2c00d6131a528b27a4a50f58b15ba9bbc
Alpha-T30 <enamult300@gmail.com>
2022-08-20 17:59:14 +0600

    registration page modified

--- client/src/screens/login.jsx
+++ client/src/screens/login.jsx
@@ -19,7 +19,6 @@ import {
   NativeBaseProvider,
   Spinner,
   useToast,
-   
 } from "native-base";
 import { loginSuccess } from "../redux/authSlice";
 import { Image, ImageBackground, StyleSheet, View } from "react-native";
@@ -32,26 +31,19 @@ const Login = ({ navigation }) => {
 
   const onSubmit = () => {
     console.log(formData);
-    logIntoApp(dispatch, formData, setLoading, toast,navigation);
+    logIntoApp(dispatch, formData, setLoading, toast, navigation);
   };
   return (
     <ImageBackground
       source={require("../images/loginbg3.jpg")}
       style={styles.maincontainer}
     >
-       
-    <View style={styles.logoimg}>
-    <Image
-
-        style={styles.logo}
-        source={require('../images/logo1.png')}
-      />
-    </View>
-    <Center w="100%">
-    
-      <Box safeArea p="2" py="8" w="90%" maxW="290">
-      
-        {/* <Heading
+      <View style={styles.logoimg}>
+        <Image style={styles.logo} source={require("../images/logo1.png")} />
+      </View>
+      <Center w="100%">
+        <Box safeArea p="2" py="8" w="90%" maxW="290">
+          {/* <Heading
           size="lg"
           fontWeight="600"
           color="coolGray.800"
@@ -61,93 +53,97 @@ const Login = ({ navigation }) => {
         >
           Welcome
         </Heading> */}
-         
 
-        <VStack space={3} mt="5">
-          <FormControl>
-            <FormControl.Label>Student ID</FormControl.Label>
-            <Input
-              onChangeText={(value) =>
-                setData({ ...formData, studentId: value })
-              }
-            />
-          </FormControl>
-          <FormControl>
-            <FormControl.Label >Password</FormControl.Label>
-            <Input
-              onChangeText={(value) =>
-                setData({ ...formData, password: value })
-              }
-              type="password"
-            />
-          </FormControl>
-          <Button onPress={onSubmit} mt="2" h={"10"} colorScheme="indigo">
-            {isLoading ? (
-              <Wave
-                animating={isLoading}
-                hidesWhenStopped={isLoading}
-                size={34}
-                color="#FFF"
-              ></Wave>
-            ) : (
-              "Log In"
-            )}
-          </Button>
-          <HStack mt="6" justifyContent="center">
-            <Text
-              fontSize="sm"
-              color="coolGray.600"
-              _dark={{
-                color: "warmGray.200",
-              }}
-            >
-              I'm a new user.{" "}
-            </Text>
-            <Text
-              style={{
-                color: "blue",
-                textDecorationLine: "underline",
-              }}
-              onPress={() => {
-                navigation.navigate("Register");
-              }}
-            >
-              Sign Up
-            </Text>
-          </HStack>
-        </VStack>
-      </Box>
-    </Center>
+          <VStack space={3} mt="5">
+            <FormControl>
+              <FormControl.Label
+                _text={{
+                  bold: true,
+                }}
+              >
+                Student ID
+              </FormControl.Label>
+              <Input
+                onChangeText={(value) =>
+                  setData({ ...formData, studentId: value })
+                }
+              />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label
+                _text={{
+                  bold: true,
+                }}
+              >
+                Password
+              </FormControl.Label>
+              <Input
+                onChangeText={(value) =>
+                  setData({ ...formData, password: value })
+                }
+                type="password"
+              />
+            </FormControl>
+            <Button onPress={onSubmit} mt="2" h={"10"} colorScheme="indigo">
+              {isLoading ? (
+                <Wave
+                  animating={isLoading}
+                  hidesWhenStopped={isLoading}
+                  size={34}
+                  color="#FFF"
+                ></Wave>
+              ) : (
+                "Log In"
+              )}
+            </Button>
+            <HStack mt="6" justifyContent="center">
+              <Text
+                fontSize="sm"
+                color="coolGray.600"
+                _dark={{
+                  color: "warmGray.800",
+                }}
+              >
+                I'm a new user.{" "}
+              </Text>
+              <Text
+                style={{
+                  color: "blue",
+                  textDecorationLine: "underline",
+                }}
+                onPress={() => {
+                  navigation.navigate("Register");
+                }}
+              >
+                Sign Up
+              </Text>
+            </HStack>
+          </VStack>
+        </Box>
+      </Center>
     </ImageBackground>
-
   );
 };
 
 export default Login;
 
-
 const styles = StyleSheet.create({
   maincontainer: {
     flex: 1,
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center'
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   container: {
     flex: 1,
   },
-  logoimg:{
-     
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
-     
-    
-   
-  } , 
-  logo:{
-    width:150,
-    height:150
-  }
-   
+  logoimg: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 150,
+    height: 150,
+  },
 });
--- client/src/screens/register.jsx
+++ client/src/screens/register.jsx
@@ -22,7 +22,13 @@ import SearchableDropdown from "react-native-searchable-dropdown";
 import { bloodGroup, districts, subDistricts } from "../../Data/data";
 import SearchAndPic from "./SearchAndPick";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Dimensions, ImageBackground, LogBox, StyleSheet, View } from "react-native";
+import {
+  Dimensions,
+  ImageBackground,
+  LogBox,
+  StyleSheet,
+  View,
+} from "react-native";
 import { AutocompleteDropdown } from "react-native-autocomplete-dropdown";
 import { register } from "../redux/apiCalls";
 const Register = ({ navigation }) => {
@@ -30,8 +36,16 @@ const Register = ({ navigation }) => {
   const [bloodGroupselect, setBloodGroup] = React.useState([]);
   const [subDistrict, setSubDistrict] = React.useState([]);
   const [singleSub, setSingleSub] = React.useState(null);
-  const [formData, setData] = React.useState({});
+  const [formData, setData] = React.useState({
+    name: "",
+    mobile: "",
+    studentId: "",
+    seassion: "",
+    password: "",
+    confirmPass: "",
+  });
   const [error, setError] = React.useState(false);
+  const [errors, setErrors] = React.useState({});
   const [isLoading, setLoading] = React.useState(false);
   const toast = useToast();
 
@@ -39,7 +53,7 @@ const Register = ({ navigation }) => {
     LogBox.ignoreLogs(["VirtualizedLists should never be nested"]);
   }, []);
 
-  const screenWidth = Dimensions.get('window').width;
+  const screenWidth = Dimensions.get("window").width;
   const handleDistrictChange = (item) => {
     setDistrict(item);
     const temp = subDistricts.filter((i) => i.districtId === item);
@@ -52,11 +66,26 @@ const Register = ({ navigation }) => {
     //  setSubDistrict(temp)
   };
 
+  //validation
+  const validate = (sendingData) => {
+    if (
+      sendingData?.name === undefined ||
+      sendingData?.name.length < 3 ||
+      sendingData?.mobile?.length !== 11 ||
+      sendingData?.studentId.length !== 10 ||
+      sendingData?.password.length < 6
+    ) {
+      return false;
+    }
+
+    return true;
+  };
+
   const onSubmit = () => {
     const findblood = bloodGroup.find((i) => i.id === bloodGroupselect);
     // const findDistrict = districts.find((i) => i.id === districtSelect);
     // const findSubDistrict = subDistricts.find((i) => i.id === singleSub);
-    if (formData.password !== formData.confirmPass) {
+    if (formData?.password !== formData?.confirmPass) {
       setError(true);
     } else {
       const sendingData = {
@@ -72,222 +101,219 @@ const Register = ({ navigation }) => {
         // subDistrict: findSubDistrict.name,
       };
       console.log(sendingData);
-      register(sendingData, setLoading, navigation, toast);
+
+      validate(sendingData)
+        ? register(sendingData, setLoading, navigation, toast)
+        : toast.show({
+            placement: "top",
+            title: "Validation Failed",
+          });
     }
   };
   return (
-     
-    <Center w="100%">  
+    <Center w="100%">
+      <Stack mt="1">
+        <ScrollView
+          h="100%"
+          w={screenWidth}
+          bg={"red.100"}
+          mt="7"
+          _contentContainerStyle={{
+            alignItems: "center",
 
- 
-
-      
-        
-         
-
-        <Stack mt="1">
-          
-        
-      <ScrollView
-        h="100%"
-        w={screenWidth}
-        bg={'red.100'}
-        mt='7'
-       
-       
-         
-        _contentContainerStyle={{
-          alignItems:'center',
-        
-        
-          // px: "90px",
-          // mb: "4",
-          // minW:`${screenWidth}`
-        }}
-      >
-        <Box  safeArea p="2" w="90%" maxW="290" py="8">
-          <VStack space={3}>
-            <FormControl>
-              <FormControl.Label>Name</FormControl.Label>
-              <Input
-                onChangeText={(value) => setData({ ...formData, name: value })}
-              />
-
-              
-            </FormControl>
-            <FormControl>
-              <FormControl.Label>Mobile</FormControl.Label>
-              <Input
-                onChangeText={(value) =>
-                  setData({ ...formData, mobile: value })
-                }
-              />
-              <FormControl.HelperText>
-              Must be atleast 11 characters and start with 01
-            </FormControl.HelperText>
-            </FormControl>
-            <FormControl>
-              <FormControl.Label >Student ID</FormControl.Label>
-              <Input
-                onChangeText={(value) =>
-                  setData({ ...formData, studentId: value })
-                }
-              />
-              <FormControl.HelperText>
-              Must be atleast 10 characters.
-            </FormControl.HelperText>
-            </FormControl>
-            <FormControl>
-              <FormControl.Label>Session</FormControl.Label>
-              <Input
-                onChangeText={(value) =>
-                  setData({ ...formData, session: value })
-                }
-              />
-            </FormControl>
-            <FormControl>
-              <FormControl.Label>Blood Group</FormControl.Label>
-              <Select
-                selectedValue={bloodGroupselect}
-                minWidth="200"
-                accessibilityLabel="Select Blood Group"
-                placeholder="Select Blood Group"
-                _selectedItem={{
-                  bg: "teal.600",
-                  endIcon: <CheckIcon size="5" />,
-                }}
-                mt={1}
-                onValueChange={(itemValue) => setBloodGroup(itemValue)}
-              >
-                {bloodGroup.map((item, i) => {
-                  return (
-                    <Select.Item key={i} label={item.name} value={item.id} />
-                  );
-                })}
-              </Select>
-            </FormControl>
-            {/* <FormControl>
-              <FormControl.Label>Select District</FormControl.Label>
-              <Select
-                selectedValue={districtSelect}
-                minWidth="200"
-                accessibilityLabel="Select District"
-                placeholder="Select District"
-                _selectedItem={{
-                  bg: "teal.600",
-                  endIcon: <CheckIcon size="5" />,
-                }}
-                mt={1}
-                onValueChange={(itemValue) => handleDistrictChange(itemValue)}
-              >
-                {districts.map((item, i) => {
-                  return (
-                    <Select.Item key={i} label={item.name} value={item.id} />
-                  );
-                })}
-              </Select>
-            </FormControl> */}
-            {/* <FormControl>
-              <FormControl.Label>Select Sub-District</FormControl.Label>
-              <Select
-                selectedValue={singleSub}
-                minWidth="200"
-                accessibilityLabel="Select Sub-District"
-                placeholder="Select Sub-District"
-                _selectedItem={{
-                  bg: "teal.600",
-                  endIcon: <CheckIcon size="5" />,
-                }}
-                mt={1}
-                onValueChange={(itemValue) => handleSubDistrict(itemValue)}
-              >
-                {subDistrict.map((item, i) => {
-                  return (
-                    <Select.Item key={i} label={item.name} value={item.id} />
-                  );
-                })}
-              </Select>
-            </FormControl> */}
-
-            <FormControl isRequired>
-              <Stack>
-
-              <FormControl.Label>Password</FormControl.Label>
-              <Input
-                onChangeText={(value) =>
-                  setData({ ...formData, password: value })
-                }
-                type="password"
+            // px: "90px",
+            // mb: "4",
+            // minW:`${screenWidth}`
+          }}
+        >
+          <Box safeArea p="2" w="90%" maxW="290" py="8">
+            <VStack space={3}>
+              <FormControl isRequired>
+                <FormControl.Label
+                  _text={{
+                    bold: true,
+                  }}
+                >
+                  Name
+                </FormControl.Label>
+                <Input
+                  onChangeText={(value) =>
+                    setData({ ...formData, name: value })
+                  }
                 />
-
-<FormControl.HelperText>
-              Must be atleast 6 characters.
-            </FormControl.HelperText>
-            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-              Atleast 6 characters are required.
-            </FormControl.ErrorMessage>
-                </Stack>
-            </FormControl>
-
-
-            <FormControl isRequired>
-              <Stack>
-
-              <FormControl.Label>Confirm Password</FormControl.Label>
-              <Input
-                onChangeText={(value) =>
-                  setData({ ...formData, confirmPass: value })
+                {
+                  <Text style={{ color: "blue", fontSize: 12 }}>
+                    {formData?.name.length < 3
+                      ? "Name Must be at least 3 character"
+                      : ""}
+                  </Text>
                 }
-                type="password"
+              </FormControl>
+              <FormControl isRequired>
+                <FormControl.Label
+                  _text={{
+                    bold: true,
+                  }}
+                >
+                  Mobile
+                </FormControl.Label>
+                <Input
+                  onChangeText={(value) =>
+                    setData({ ...formData, mobile: value })
+                  }
                 />
-<FormControl.HelperText>
-              Must be atleast 6 characters.
-            </FormControl.HelperText>
-            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-              Atleast 6 characters are required.
-            </FormControl.ErrorMessage>
+                {
+                  <Text style={{ color: "blue", fontSize: 12 }}>
+                    {formData?.mobile?.length !== 11
+                      ? "Phone Number Must be  of 11 Digits"
+                      : ""}
+                  </Text>
+                }
+              </FormControl>
+              <FormControl
+                isRequired
+                _text={{
+                  bold: true,
+                }}
+              >
+                <FormControl.Label>Student ID</FormControl.Label>
+                <Input
+                  onChangeText={(value) =>
+                    setData({ ...formData, studentId: value })
+                  }
+                />
+                {
+                  <Text style={{ color: "blue", fontSize: 12 }}>
+                    {formData?.studentId?.length !== 10
+                      ? "Student ID Must be at least of 10 digits"
+                      : ""}
+                  </Text>
+                }
+              </FormControl>
+              <FormControl>
+                <FormControl.Label
+                  _text={{
+                    bold: true,
+                  }}
+                >
+                  Session
+                </FormControl.Label>
+                <Input
+                  onChangeText={(value) =>
+                    setData({ ...formData, session: value })
+                  }
+                />
+              </FormControl>
+              <FormControl isRequired={true}>
+                <FormControl.Label
+                  _text={{
+                    bold: true,
+                  }}
+                >
+                  Blood Group
+                </FormControl.Label>
+                <Select
+                  selectedValue={bloodGroupselect}
+                  minWidth="200"
+                  accessibilityLabel="Select Blood Group"
+                  placeholder="Select Blood Group"
+                  _selectedItem={{
+                    bg: "teal.600",
+                    endIcon: <CheckIcon size="5" />,
+                  }}
+                  mt={1}
+                  onValueChange={(itemValue) => setBloodGroup(itemValue)}
+                >
+                  {bloodGroup.map((item, i) => {
+                    return (
+                      <Select.Item key={i} label={item.name} value={item.id} />
+                    );
+                  })}
+                </Select>
+              </FormControl>
 
+              <FormControl isRequired>
+                <Stack>
+                  <FormControl.Label
+                    _text={{
+                      bold: true,
+                    }}
+                  >
+                    Password
+                  </FormControl.Label>
+                  <Input
+                    onChangeText={(value) =>
+                      setData({ ...formData, password: value })
+                    }
+                    type="password"
+                  />
+
+                  {
+                    <Text style={{ color: "blue", fontSize: 12 }}>
+                      {formData?.password?.length < 6
+                        ? "Password Must be at least 6 characters"
+                        : ""}
+                    </Text>
+                  }
                 </Stack>
-            </FormControl>
-            <Button onPress={onSubmit} mt="2" colorScheme="indigo">
-              {isLoading ? <Spinner color="warning.500" /> : "Sign up"}
-            </Button>
+              </FormControl>
 
-            <Text style={{ color: "red" }}>
-              {error ? "Password Does Not Matched" : ""}
-            </Text>
-            <HStack mt="6" justifyContent="center">
-              <Text
-                fontSize="sm"
-                color="coolGray.600"
-                _dark={{
-                  color: "warmGray.200",
-                }}
-              >
-                Already a user?
-              </Text>
-              <Text
-                style={{
-                  color: "blue",
-                  textDecorationLine: "underline",
-                }}
-                onPress={() => {
-                  navigation.goBack();
-                }}
-              >
-                Login
-              </Text>
-            </HStack>
-          </VStack>
-        </Box>
-      </ScrollView>
-      
+              <FormControl isRequired>
+                <Stack>
+                  <FormControl.Label
+                    _text={{
+                      bold: true,
+                    }}
+                  >
+                    Confirm Password
+                  </FormControl.Label>
+                  <Input
+                    onChangeText={(value) =>
+                      setData({ ...formData, confirmPass: value })
+                    }
+                    type="password"
+                  />
+                  <Text style={{ color: "blue", fontSize: 12 }}>
+                    {formData?.confirmPass?.length < 6
+                      ? "Password Must be at least 6 characters"
+                      : ""}
+                  </Text>
+                </Stack>
+              </FormControl>
+              <Button onPress={onSubmit} mt="2" colorScheme="indigo">
+                {isLoading ? <Spinner color="warning.500" /> : "Sign up"}
+              </Button>
 
+              <Text style={{ color: "red" }}>
+                {error ? "Password Does Not Matched" : ""}
+              </Text>
+              <HStack mt="6" justifyContent="center">
+                <Text
+                  fontSize="sm"
+                  color="coolGray.600"
+                  _dark={{
+                    color: "warmGray.200",
+                  }}
+                >
+                  Already a user?
+                </Text>
+                <Text
+                  style={{
+                    color: "blue",
+                    textDecorationLine: "underline",
+                  }}
+                  onPress={() => {
+                    navigation.goBack();
+                  }}
+                >
+                  Login
+                </Text>
+              </HStack>
+            </VStack>
+          </Box>
+        </ScrollView>
       </Stack>
-       
     </Center>
-
-     
   );
 };
 
@@ -296,12 +322,11 @@ export default Register;
 const styles = StyleSheet.create({
   maincontainer: {
     flex: 1,
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
-    width:'100%',
-    marginLeft:15,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    marginLeft: 15,
     // backgroundColor:'red'
-  }
-   
+  },
 });
